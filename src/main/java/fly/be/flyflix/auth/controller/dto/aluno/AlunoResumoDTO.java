3daf0e5d6527b0d3304f5d4bf0137ac3b549e94f
mbranches <marcusvbranches@gmail.com>
2025-06-20 21:22:16 -0300

    feat: adiciona atributo email a AlunoResumoDTO

--- src/main/java/fly/be/flyflix/auth/controller/dto/aluno/AlunoResumoDTO.java
+++ src/main/java/fly/be/flyflix/auth/controller/dto/aluno/AlunoResumoDTO.java
@@ -2,9 +2,9 @@ package fly.be.flyflix.auth.controller.dto.aluno;
 
 import fly.be.flyflix.auth.entity.Aluno;
 
-public record AlunoResumoDTO(Long id, String nome, Boolean ativo) {
+public record AlunoResumoDTO(Long id, String nome, String email, Boolean ativo) {
     public AlunoResumoDTO(Aluno aluno) {
-        this(aluno.getId(), aluno.getNome(), aluno.getAtivo());
+        this(aluno.getId(), aluno.getNome(), aluno.getEmail(), aluno.getAtivo());
     }
 }
 
