446dced54ff73b0520287233238cce802a562282
mbranches <marcusvbranches@gmail.com>
2025-06-22 23:38:06 -0300

    feat: adiciona tratamento global de exceções

---
+++ src/main/java/fly/be/flyflix/conteudo/exceptions/DefaultMessageError.java
@@ -0,0 +1,3 @@
+package fly.be.flyflix.conteudo.exceptions;
+
+public record DefaultMessageError(int status, String message) {}
---
+++ src/main/java/fly/be/flyflix/conteudo/exceptions/GlobalErrorHandlerAdvice.java
@@ -0,0 +1,22 @@
+package fly.be.flyflix.conteudo.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalErrorHandlerAdvice {
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<DefaultMessageError> handlerBadRequestException(BadRequestException e) {
+        DefaultMessageError error = new DefaultMessageError(e.getStatusCode().value(), e.getReason());
+
+        return ResponseEntity.status(e.getStatusCode()).body(error);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<DefaultMessageError> handlerNotFoundException(NotFoundException e) {
+        DefaultMessageError error = new DefaultMessageError(e.getStatusCode().value(), e.getReason());
+
+        return ResponseEntity.status(e.getStatusCode()).body(error);
+    }
+}
