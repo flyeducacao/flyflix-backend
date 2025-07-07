e9eb7198e8c857f16363f75271e3d6e6255775c0
mbranches <marcusvbranches@gmail.com>
2025-07-07 14:15:09 -0300

    remove: remove atributo autorId do dto CadastroCurso

--- src/main/java/fly/be/flyflix/conteudo/dto/curso/CadastroCurso.java
+++ src/main/java/fly/be/flyflix/conteudo/dto/curso/CadastroCurso.java
@@ -1,13 +1,7 @@
 package fly.be.flyflix.conteudo.dto.curso;
 
-import fly.be.flyflix.conteudo.dto.modulo.CadastroModulo;
-import java.util.List;
-
-import java.util.List;
-
 public record CadastroCurso(
-        String titulo,
-        Long autorId
+        String titulo
 ) {}
 
 
