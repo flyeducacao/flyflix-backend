24f8c2e325d4b2fd3110874d9348aab5259f081a
mbranches <marcusvbranches@gmail.com>
2025-07-08 11:44:49 -0300

    remove: remove imports, annotations e atributos desnecessários

--- src/main/java/fly/be/flyflix/conteudo/service/ModuloService.java
+++ src/main/java/fly/be/flyflix/conteudo/service/ModuloService.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -37,7 +36,6 @@ public class ModuloService {
         moduloRepository.save(modulo);
     }
 
-    @Transactional
     public void remover(Long id) {
         moduloRepository.deleteById(id);
     }
