22505b4479b4a94217c8a8c764bf9000a31b5ee7
Valdirene Verdum <vverdum@gmail.com>
2025-12-08 20:32:11 -0300

    "novo banco"

--- src/main/java/fly/be/flyflix/config/DefaultAdminUserConfig.java
+++ src/main/java/fly/be/flyflix/config/DefaultAdminUserConfig.java
@@ -14,11 +14,20 @@ import java.time.LocalDate;
 
 @Configuration
 public class DefaultAdminUserConfig implements CommandLineRunner {
-    @Autowired
-    private AdminRepository adminRepository;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-    private UsuarioService usuarioService;
+
+    private final AdminRepository adminRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UsuarioService usuarioService;
+
+    public DefaultAdminUserConfig(
+            AdminRepository adminRepository,
+            PasswordEncoder passwordEncoder,
+            UsuarioService usuarioService
+    ) {
+        this.adminRepository = adminRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.usuarioService = usuarioService;
+    }
 
     @Override
     @Transactional
@@ -26,23 +35,24 @@ public class DefaultAdminUserConfig implements CommandLineRunner {
 
         final String adminEmail = "dev_educ@flyeducacao.org";
         final String adminCpf = "68669859432";
+
         System.out.println("Buscando admin no banco...");
         var adminOptional = adminRepository.findByEmail(adminEmail);
         System.out.println("Resultado: " + adminOptional);
+
         adminOptional.ifPresentOrElse(
                 admin -> System.out.println("Admin já existe no banco"),
                 () -> {
                     Admin admin = new Admin();
                     admin.setNome("Admin");
                     admin.setEmail(adminEmail);
-
                     admin.setCpf(adminCpf);
-
-                    admin.setSenha(passwordEncoder.encode("FlyAdmin*8")); // senha padrão
+                    admin.setSenha(passwordEncoder.encode("FlyAdmin*8"));
                     admin.setAtivo(true);
                     admin.setRole(Role.ADMIN);
-                    //admin.setDataNascimento(LocalDate.of(2022, 1, 17));
+
                     usuarioService.adicionarFotoDePerfilPadrao(admin);
+
                     adminRepository.save(admin);
                     System.out.println("Admin padrão criado com sucesso");
                 }
